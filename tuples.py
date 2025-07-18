312a0492f7b3ec6ad7bffde518ddcf12d53b8b0a
Derek Sathre <164694886+UpinSmoke6@users.noreply.github.com>
2025-07-18 17:50:01 -0500

    Delete tuples.py

--- tuples.py
+++
@@ -1,7 +0,0 @@
-short_tuple = "rolf", "bob"
-a_bit_clearer = ("Rolf", "Bob")
-
-# tuples are immutable. Lists can be modified.
-friends = ("Rolf", "Bob", "Anne")
-friends = friends + ("Jen",)
-print(friends)
